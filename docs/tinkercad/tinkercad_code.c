4699036a4e035b98f334041861f78b03ad8b61b3
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-30 12:29:34 +0100

    feat/tinkercad: added simulation mode toggle and cleaned codce

--- docs/tinkercad/tinkercad_code.c
+++ docs/tinkercad/tinkercad_code.c
@@ -2,15 +2,20 @@
 #define RED_LED 4
 #define GREEN_LED 3
 
-#define BUTTON_PIN 2
+#define TEMP_TOGGLE_PIN 2
+#define SIM_MODE_PIN 5
 
 #define TMP_PIN A0
 
-bool previousButtonState = 1;
-bool showCelsius = 1;
-bool showFarenheit = 0;
+bool previousTempToggleButton = HIGH;
+bool showFarenheit = false;
 
-void switchtTemp();
+bool previousSimModeButton = HIGH;
+bool simModeForced = false;
+
+bool sensorValid(double tempC) {
+  return tempC > -40 && tempC < 125;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -20,41 +25,53 @@ void setup() {
   pinMode(GREEN_LED, OUTPUT);
   
   // Setup button
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(TEMP_TOGGLE_PIN, INPUT_PULLUP);
+  pinMode(SIM_MODE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
+  // Toggle temp unit when button clicked
+  bool tempToggleButton = digitalRead(TEMP_TOGGLE_PIN);
+  if (previousTempToggleButton == HIGH && tempToggleButton == LOW) {
+    showFarenheit = !showFarenheit;
+  }
+  previousTempToggleButton = tempToggleButton;
+  
+  // Toggle simulation mode when button clicked
+  bool simModeButton = digitalRead(SIM_MODE_PIN);
+  if (previousSimModeButton == HIGH && simModeButton == LOW) {
+    simModeForced = !simModeForced;
+  }
+  previousSimModeButton = simModeButton;
+  
   int TMPValue = analogRead(TMP_PIN);
   // Read temp according to voltage (related to TMP)
-  double Voltage = (TMPValue / 1023.0) * 5000;
-  double tempC = (Voltage - 500) * 0.1;
-  double tempF = (tempC * 1.8) + 32;
+  double voltage = (TMPValue / 1023.0) * 5000;
+  double tempC = (voltage - 500) * 0.1;
   
-  bool buttonState = digitalRead(BUTTON_PIN);
+  bool isValidSensor = sensorValid(tempC);
   
-  if (buttonState != previousButtonState) {
-    if (buttonState == HIGH) {
-      // Toggle temp mode
-      showCelsius = !showCelsius;
-      showFarenheit = !showFarenheit;
-      
-      // Turn Leds off
-      digitalWrite(RED_LED, LOW);
-      digitalWrite(GREEN_LED, LOW);
-    }
-    
-  	previousButtonState = buttonState;
-  }
+  bool simMode = simModeForced || !isValidSensor;
   
-  // display celsius temperature + turning led on
-  if (showCelsius) {
-    Serial.println(tempC);
-    digitalWrite(RED_LED, HIGH);
-  }
+  // Turn Leds off
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
   
-  // display farenheit temperature + turning led on
+  // display temperature + turning led on
   if (showFarenheit) {
-    Serial.println(tempF);
-    digitalWrite(GREEN_LED, HIGH);
+    if (simMode) {
+      Serial.println(77);
+    } else {
+      double tempF = (tempC * 1.8) + 32;
+      Serial.println(tempF);
+      digitalWrite(GREEN_LED, HIGH);
+    }  
+  } else {
+    if (simMode) {
+      Serial.println(25);
+    } else {
+      Serial.println(tempC);
+      digitalWrite(RED_LED, HIGH);  
+    }  
   }
 }
