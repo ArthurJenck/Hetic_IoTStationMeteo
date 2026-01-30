69e5a169b114d6b5f65fc19a8db1a28dd9ff5c46
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-30 10:28:27 +0100

    initial schema

---
+++ docs/tinkercad/tinkercad_code.c
@@ -0,0 +1,16 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  // delay(1000); // Wait for 1000 millisecond(s)
+  // digitalWrite(3, LOW);
+  // delay(1000); // Wait for 1000 millisecond(s)
+}
