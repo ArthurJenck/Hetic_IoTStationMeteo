c83f5817032d711abd95d094ac79e20beaa7e791
Alexis-Gontier <alexis.gontier03@gmail.com>
2026-01-30 10:09:57 +0100

    🚀init frontend

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { CenteredLayout } from "@/components/layout/centered-layout";
+
+export default function NotFound() {
+    return (
+        <CenteredLayout>
+            404 - Not Found
+        </CenteredLayout>
+    );
+}
